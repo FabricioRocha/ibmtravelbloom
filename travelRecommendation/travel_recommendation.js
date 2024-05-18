fd76fb295d81076909b6959641acec5ec9caf594
Fabricio Rocha <19875696+FabricioRocha@users.noreply.github.com>
2024-05-18 09:08:19 -0300

    Card duplication, file reading code for debugging

---
+++ travelRecommendation/travel_recommendation.js
@@ -0,0 +1,46 @@
+var db = {};
+
+let opts = {
+    method: "GET",
+    mode: "no-cors"
+}
+fetch("travel_recommendation_api.json", opts)
+    .then(
+        (result) => {
+            console.log("Database loaded");
+            db = result.json();
+        }
+    )
+    .catch (
+        (error) => {
+            console.log ("Something wrong importing the JSON:");
+            console.log (error.message);
+        }
+    )
+
+
+function dbSearch (keyword) {
+    if (keyword == "") return;
+
+    if (keyword.toLowerCase().includes("beach")) {
+        return db.beaches;
+    } 
+    
+    if (keyword.toLowerCase().includes("temple")) {
+        return db.temples;
+    }
+
+    if (keyword.toLowerCase().includes("countr")) {
+        let cities = []
+        for (let i = 0; i < db.countries.length; i++) {
+            for (let j = 0; j < db.countries.cities.length; j++) {
+                cities.push(db.countries[i].cities[j]);
+            } 
+        }
+
+        return cities;
+    }
+
+    return [];
+}
+
